f6ffe00de70c029b9c02263a29af32f9d5eeca3c
Petr Podrabsky <petrpo@gmail.com>
2013-11-13 12:32:48 +0100

    ADD: ssh to installed apps

--- debian-basic.sh
+++ debian-basic.sh
@@ -2,6 +2,6 @@
 
 SYSTEM=`cat /etc/issue.net`
 sudo apt-get update
-sudo apt-get -y install mc aptitude iftop tcpdump xterm vim
+sudo apt-get -y install mc aptitude iftop tcpdump xterm vim ssh
 
 echo "System $SYSTEM is unified :-)"
