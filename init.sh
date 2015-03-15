7d801dd1facc69a2972a1f95545d72e1adc5f792
Petr Podrabsky <petrpo@gmail.com>
2015-03-15 21:05:35 +0100

    UPDATE : added openssh-server

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 SYSTEM=`cat /etc/issue.net`
 sudo apt-get update
-sudo apt-get -y install mc aptitude iftop tcpdump xterm vim ssh rsync git htop
+sudo apt-get -y install mc aptitude iftop tcpdump xterm vim openssh-server openssh rsync git htop
 
 echo "System $SYSTEM is unified :-)"
