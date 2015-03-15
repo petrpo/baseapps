d3ed77e4f10f74a1efd0258451f797ea7d98c760
Petr Podrabsky <petrpo@gmail.com>
2015-03-15 21:03:30 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 SYSTEM=`cat /etc/issue.net`
 sudo apt-get update
-sudo apt-get -y install mc aptitude iftop tcpdump xterm vim ssh
+sudo apt-get -y install mc aptitude iftop tcpdump xterm vim ssh rsync git htop
 
 echo "System $SYSTEM is unified :-)"
