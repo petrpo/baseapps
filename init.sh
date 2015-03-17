693c45fbd8b53a672517c55206b113491cab5797
Petr Podrabsky <petrpo@gmail.com>
2015-03-17 17:02:02 +0100

    UPDATE: add screen traceroute

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 SYSTEM=`cat /etc/issue.net`
 sudo apt-get update
-sudo apt-get -y install mc aptitude iftop tcpdump vim openssh-server rsync git htop
+sudo apt-get -y install mc aptitude iftop tcpdump vim openssh-server rsync git htop screen traceroute
 
 echo "System $SYSTEM is unified :-)"
