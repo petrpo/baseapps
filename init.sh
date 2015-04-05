75e826b68ada6477f454d387b84dfde844e65647
_petrpo_ <petrpo@gmail.com>
2015-04-05 09:33:32 +0200

    UPDATE : script updateed with server and desktop tunnings

--- init.sh
+++ init.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-SYSTEM=`cat /etc/issue.net`
-sudo apt-get update
-sudo apt-get -y install mc aptitude iftop tcpdump vim openssh-server rsync git htop screen traceroute
+SERVER_APPS="mc aptitude iftop tcpdump vim openssh-server rsync htop screen traceroute xterm"
+DESKTOP_APPS="keepass2 vim-gtk"
 
-echo "System $SYSTEM is unified :-)"
+# run under root privileges
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script" 2>&1
+  exit 1
+fi
+
+sudo apt-get update
+
+if [ "$XDG_CURRENT_DESKTOP" = "" ]
+then
+  echo "Let's unify server"
+  echo
+  sudo apt-get -y install $SERVER_APPS
+else
+  echo "Let's unify Desktop"
+  echo
+  sudo apt-get -y install $SERVER_APPS
+  sudo apt-get -y install $DESKTOP_APPS
+fi
+
+HOST_VERSION=`cat /etc/issue.net`
+
+echo "$HOST_VERSION is unified according to universe standards :-)"
+echo
